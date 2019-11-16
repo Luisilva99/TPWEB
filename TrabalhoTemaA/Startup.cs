44120d6a8bde5c84aff4224e8f63269283e27bfd
Luisilva99 <44380629+Luisilva99@users.noreply.github.com>
2019-11-16 14:36:52 +0000

    Inicio do Trabalho

---
+++ TrabalhoTemaA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrabalhoTemaA.Startup))]
+namespace TrabalhoTemaA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
